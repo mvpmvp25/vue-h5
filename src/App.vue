7e5a223bb2ad544c0f25caa83ff0724434371dc4
fizz <441980627@qq.com>
2017-08-08 16:10:36 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,60 @@
 <template>
-	<div>
-		<transition name="router-fade" mode="out-in">
-			<keep-alive>
-			    <router-view v-if="$route.meta.keepAlive"></router-view>
-			</keep-alive>
-    	</transition>
-    	<transition name="router-fade" mode="out-in">
-			<router-view v-if="!$route.meta.keepAlive"></router-view>
-		</transition>
-		<svg-icon></svg-icon>	
-    </div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-	import svgIcon from './components/common/svg';
-  	export default {
-    	components:{
-            svgIcon
-        },
-  	}
-
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: '项目构建'
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-  	@import './style/common';
-	.router-fade-enter-active, .router-fade-leave-active {
-	  	transition: opacity .3s;
-	}
-	.router-fade-enter, .router-fade-leave-active {
-	  	opacity: 0;
-	}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
